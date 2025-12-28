a67ca0fcad72c2c6bc619e18e4fa7f2c66d273d6
angeloyolo <angelo.you@studenti.unipd.it>
2025-12-28 14:59:37 +0100

    Update Highway.h

--- include/Highway.h
+++ include/Highway.h
@@ -22,11 +22,12 @@ public:
 private:
     std::vector<Point> points;
     bool confrontaPerKm(const Point& a, const Point& b);
-    bool isDouble(const std::string& s)
+    bool isDouble(const std::string& s);
 };
 
 
 #endif
+
 
 
 
