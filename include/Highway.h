0f3e50a633ff617b84f482a3e330379b013d5544
giummocs <gianmaria.stefani@studenti.unipd.it>
2025-12-28 15:36:03 +0100

    Update Highway.h

--- include/Highway.h
+++ include/Highway.h
@@ -11,19 +11,21 @@ public:
 
     bool loadFromFile(const std::string& filename);
     std::vector<Point> getVarchiInRange(double startKm, double endKm) const;
-    const std::vector<Point>& getPoints();
+    const std::vector<Point>& getSvincoli();
+    const std::vector<Point>& getVarchi();
 
 private:
     //Abbiamo scelto di implementare due vettori per differenziare al meglio varchi e svincoli, 
     //il che facilita molto la logica negli altri file: codice piu facile da leggere e alleggerito nella ricerca di varchi e svincoli.
     std::vector<double> svincoli;
     std::vector<double> varchi;
-    bool confrontaPerKm(const Point& a, const Point& b);
+
     bool isDouble(const std::string& s);
 };
 
 
 #endif
+
 
 
 
