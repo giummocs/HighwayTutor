3f25e258c035a85d644fd401987ffdb50ca10b35
angeloyolo <angelo.you@studenti.unipd.it>
2025-12-29 10:15:44 +0100

    Update Highway.h

--- include/Highway.h
+++ include/Highway.h
@@ -4,6 +4,11 @@
 #include <vector>
 #include <string>
 
+struct VarchiAdiacenti {
+    int prev;
+    int next;
+}
+
 class Highway {
 public:
     const int MAX_SVINCOLI;
@@ -17,14 +22,16 @@ public:
 private:
     //Abbiamo scelto di implementare due vettori per differenziare al meglio varchi e svincoli, al posto di una struttura dati univoca per entrambi (struct).
     //Il che facilita molto la logica negli altri file: codice piu facile da leggere e alleggerito nella ricerca di varchi e svincoli.
-    std::vector<Point> svincoli;
+    std::vector<double> svincoli;
     std::vector<double> varchi;
+    std::vector<VarchiAdiacenti> varchiAdiacenti;
 
     bool isDouble(const std::string& s);
 };
 
 
 #endif
+
 
 
 
