f026e2d707ea7db39742d4b12a6cac3dd95175c0
giummocs <gianmaria.stefani@studenti.unipd.it>
2025-12-31 02:19:10 +0100

    Add files via upload

--- src/Tutor.cpp
+++ src/Tutor.cpp
@@ -61,26 +61,40 @@ std::string set_time(int addTime){
     }
 
     double newTime = currentTime + addTime;
+    std::string output = "\nInfrazioni commesse tra gli istanti "+currentTime+" e "+newTime+":";
 
+    //Scorro la mappa con chiave plate, cosi da scorrere un veicolo per volta
     for (const auto& mapElement : passagesPlateKey) {
-        std::cout << "chiave: " << mapElement.first
-                  << ", valore: " << mapElement.second << std::endl;
-        //Gate Index 1
-        for(std::size_t gIdx1=0, gIdx2=1; gIdx2 < mapElement.second.size(); gIdx1++, gIdx2++){
 
-            double distance = std::abs(gates[mapElement.second[gIdx1].id] - gates[mapElement.second[gIdx2].id]);
-            double time = std::abs(mapElement.second[gIdx1].time - mapElement.second[gIdx2].time);
-            double averageVelocity = distance / (time/SECONDS_IN_HOURS);
+        //Scorro tutti i gate attraversati da una macchina precisa, analizzandone due consecutivi alla volta tramite gateIdx e gateIdx+1
+        for(std::size_t gateIdx=0; gateIdx+1 < mapElement.second.size(); gateIdx++){
 
-            if(averageVelocity > 130){
-                
+            double timeGate1 = mapElement.second[gateIdx].time;
+            double timeGate2 = mapElement.second[gateIdx+1].time;
+            int idGate1 = mapElement.second[gateIdx].id;
+            int idGate2 = mapElement.second[gateIdx+1].id;
+
+            //Controllo che tutti i due gate di riferimento siano dentro il range di tempo richiesto dal parametro addTime
+            if(timeGate1 > currentTime && timeGate1 < newTime && timeGate2 > currentTime && timeGate2 > newTime){
+
+                double distanceDifference = std::abs(gates[idGate1] - gates[idGate2]); 
+                double timeDifference = std::abs(timeGate1 - timeGate2);
+                double averageVelocity = distanceDifference / (timeDifference/SECONDS_IN_HOURS);
+
+                if(averageVelocity > 130){
+                    output += "\nInfrazione";
+                    output += "\nTarga: "+mapElement.first+"\nTratta: varco "+idGate1+" - varco "+idGate2+"\nVelocità media: "+averageVelocity+"\nIstante di passaggio varco "+idGate1+": "+timeGate1+"\nIstante di passaggio varco "+idGate2+": "+timeGate2+"\n";
+                }
+
             }
         }
     }
 
-
+    if(output == "\nInfrazioni commesse tra gli istanti "+currentTime+" e "+newTime+":") output = "Nessuna infrazione.";
 
     currentTime = newTime;
+
+    return output;
 }
 
 //Traduce il tempo ricevuto come parametro da stringa a intero
@@ -108,7 +122,7 @@ int decodeInput(const std::string& s){
     }
     
 
-    //Se aveva m moltiplico per 60 per convertire in secondi
+    //Se aveva m, moltiplico per 60 per convertire in secondi
     if (hasM) {
         result *= 60;
     }
@@ -116,43 +130,77 @@ int decodeInput(const std::string& s){
     return result;
 }
 
+
 int main() {
     
-    std::string command;
+    std::string input = "";
+    std::string command = "";
+    std::string parameter = "";
 
+    //Lettura del file Highway.txt
     try {
         Highway highway("Data/Highway.txt");
+        junctions = highway.getJunctions();
+        gates = highway.getGates();
     }
     catch (const std::runtime_error& e) {
         std::cerr << e.what() << std::endl;
+        return 1; //Interrompe il main
     }
 
-    junctions = highway.getJunctions();
-    gates = highway.getGates();
-    
+    //Lettura del file Passages.txt
+    try {
+        readFromFile("Data/Passages.txt");
+    }
+    catch (const std::runtime_error& e) {
+        std::cerr << e.what() << std::endl;
+        return 1; //Interrompe il main
+    }
 
-    while (std::cin >> command) {
-        if (command == "set_time") {
-            double value;
-            std::string unit;
-            std::cin >> value;
-            // Gestione m per minuti [cite: 80]
-            currentTime += value; 
-            std::cout << "Nuovo istante: " << currentTime << ". Analisi violazioni..." << std::endl;
-            // Qui andrebbe la logica di calcolo velocità media tra varchi consecutivi [cite: 87]
+    std::cout << "------BENVENUTO IN TUTOR AUTOSTRADALE------";
+    std::cout << "Inserisci un comando>>";
+
+    while (std::cin >> input) {
+        
+        //Separo il comando da eventuali parametri
+        size_t splitPoint = input.find(' ');
+        if (splitPoint < input.size()) {
+            command = input;
+            parameter  = "";
+        } else {
+            command = input.substr(0, splitPoint);
+            parameter  = input.substr(splitPoint + 1);
+        }
+
+
+        if (command == "set_time" && parameter != "") {
+            try{
+                std::cout << set_time(decodeInput(parameter));
+            }
+            catch(const std::runtime_error& e){
+                std::cerr << e.what() << std::endl;
+            }
+            
+            std::cout << "Inserisci un comando>>";
         } 
-        else if (command == "stats") {
-            std::cout << "Statistiche sistema tutor..." << std::endl; // [cite: 90]
+        else if (command == "stats" && parameter == "") {
+            
+
+            std::cout << "Inserisci un comando>>";
         } 
-        else if (command == "reset") {
-            currentTime = 0; // [cite: 89]
+        else if (command == "reset" && parameter == "") {
+
+
+            std::cout << "Inserisci un comando>>";
         }
         else if (command == "Q" || command == "q"){
-            break;
+            return 0;
         }
         else {
-            std::cout << " Commando invalido!";
+            std::cerr << "Errore! Commando invalido!";
+            std::cout << "Inserisci un comando>>";
         }
+
     }
     return 0;
 
