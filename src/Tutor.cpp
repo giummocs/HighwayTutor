1a055c9661711014b190af286a482980a095fa48
giummocs <gianmaria.stefani@studenti.unipd.it>
2025-12-31 16:54:31 +0100

    Update Tutor.cpp

--- src/Tutor.cpp
+++ src/Tutor.cpp
@@ -64,6 +64,7 @@ std::string set_time(int addTime){
     double newTime = currentTime + addTime;
     std::string output = "\nInfrazioni commesse tra gli istanti "+currentTime+" e "+newTime+":";
 
+    //Complessita' O(n), dove n e' il numero delle righe del file passages.txt
     //Scorro la mappa con chiave plate, cosi da scorrere un veicolo per volta
     for (const auto& mapElement : passagesPlateKey) {
 
@@ -206,5 +207,6 @@ int main() {
     return 0;
 
 }
+
 
 
