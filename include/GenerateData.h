6c5ab749465839656ce3561b096b7d6c255ff6d6
rigonisimone05 <rigonisimone05@gmail.com>
2026-01-03 00:14:28 +0100

    Update GenerateData.h

--- include/GenerateData.h
+++ include/GenerateData.h
@@ -15,7 +15,7 @@
 class GenerateData : public Highway{
 public:
     void startHighwaySimulation();
-
+    GenerateData(const std::string& highwayFile) : Highway(highwayFile);
 private:
     const int HOURS_IN_SECOND = 3600 ;
     const int NUM_VEHICLES = 10000;         
@@ -26,9 +26,14 @@ private:
     const int MIN_DURATION_MIN = 5;
     const int MAX_DURATION_MIN = 15;
 
-
-
-
+    std::string generatePlate();
+    int randomInt(int min, int max);
+    double randomDouble(double min, double max);    
+    void generateRunsLine(std::ofstream& outFile, const Vehicle& vehicle);
+    void generatePassages(std::ofstream& outFile, const Vehicle& vehicle, 
+                         const std::vector<double>& gates, double kmEntry, double kmExit);
+    int findFirstGate(const std::vector<double>& gates, double kmEntry);
+    void generateProfile(Vehicle& v, double totalDistance);
 };
 
 #endif
