6fc367bc3bc8fdd3a2a1ef640efb2826d2a77611
rigonisimone05 <rigonisimone05@gmail.com>
2026-01-02 14:34:21 +0100

    Update GenerateData.h

--- include/GenerateData.h
+++ include/GenerateData.h
@@ -1,1 +1,20 @@
-a
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cstdlib>  
+#include <ctime>    
+#include <cmath>    
+#include <iomanip>  
+
+#include "Vehicle.h"
+#include "Highway.h"
+
+const int HOURS_IN_SECOND = 3600 ;
+const int NUM_VEHICLES = 10000;         
+const double MIN_TIME_GAP = 0.5;        
+const double MAX_TIME_GAP = 10.0;       
+const int MIN_SPEED = 80;
+const int MAX_SPEED = 190;
+const int MIN_DURATION_MIN = 5;
+const int MAX_DURATION_MIN = 15;
