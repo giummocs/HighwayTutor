9f007045384f87fe8a6f3eee9dfd99dfbf5b48b3
rigonisimone05 <rigonisimone05@gmail.com>
2026-01-02 14:42:48 +0100

    Update GenerateData.h

--- include/GenerateData.h
+++ include/GenerateData.h
@@ -12,11 +12,20 @@
 #include "Vehicle.h"
 #include "Highway.h"
 
-const int HOURS_IN_SECOND = 3600 ;
-const int NUM_VEHICLES = 10000;         
-const double MIN_TIME_GAP = 0.5;        
-const double MAX_TIME_GAP = 10.0;       
-const int MIN_SPEED = 80;
-const int MAX_SPEED = 190;
-const int MIN_DURATION_MIN = 5;
-const int MAX_DURATION_MIN = 15;
+class GenerateData : public Highway{
+public:
+    
+
+private:
+    const int HOURS_IN_SECOND = 3600 ;
+    const int NUM_VEHICLES = 10000;         
+    const double MIN_TIME_GAP = 0.5;        
+    const double MAX_TIME_GAP = 10.0;       
+    const int MIN_SPEED = 80;
+    const int MAX_SPEED = 190;
+    const int MIN_DURATION_MIN = 5;
+    const int MAX_DURATION_MIN = 15;
+
+
+};
+
