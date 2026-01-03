78a49a49333098fde17bd6f883a791a02123cf19
angeloyolo <angelo.you@studenti.unipd.it>
2026-01-03 23:27:01 +0100

    Update Highway.cpp

--- src/Highway.cpp
+++ src/Highway.cpp
@@ -2,8 +2,8 @@
 
 Highway::Highway() {
     //Inizializzazione a valori di default 
-    nodes['V'].clear();
-    nodes['S'].clear();
+    nodes['V'];
+    nodes['S'];
 }
 
 Highway::Highway(const std::string& filename) { loadFromFile(filename); }
@@ -134,6 +134,7 @@ void Highway::printGates() {
     }
     return;
 }
+
 
 
 
