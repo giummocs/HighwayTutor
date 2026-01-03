1a3d2b452449d9f4bad4bffffd318d66b74cfaec
giummocs <gianmaria.stefani@studenti.unipd.it>
2026-01-03 18:35:49 +0100

    Add files via upload

--- src/Tutor.cpp
+++ src/Tutor.cpp
@@ -1,5 +1,19 @@
 
-#include "DataProcess.h";
+#include "DataProcessor.h";
+
+
+void splitInput(std::string input, std::string& command, std::string& parameter){
+    //Separo il comando da eventuali parametri
+    size_t splitPoint = input.find(' ');
+    if (splitPoint < input.size()) {
+        command = input.substr(0, splitPoint);
+        parameter  = input.substr(splitPoint + 1);
+    } else {
+        command = input;
+        parameter  = "";
+    }
+}
+
 
 int main() {
 
@@ -7,19 +21,11 @@ int main() {
     std::string command = "";
     std::string parameter = "";
 
-    //Lettura del file Highway.txt
-    try {
-        Highway highway("Data/Highway.txt");
-        gates = highway.getGates();
-    }
-    catch (const std::runtime_error& e) {
-        std::cerr << e.what() << std::endl;
-        return 1; //Interrompe il main
-    }
+    std::unique_ptr<DataProcessor> dp = nullptr;
 
-    //Lettura del file Passages.txt
+    //Lettura dei file Highway.txt e Passsages.txt e riempimento dei dati
     try {
-        readFromFile("Data/Passages.txt");
+        dp = std::make_unique<DataProcessor>("Data/Highway.txt","Data/Passages.txt");
     }
     catch (const std::runtime_error& e) {
         std::cerr << e.what() << std::endl;
@@ -31,20 +37,11 @@ int main() {
 
     while (std::cin >> input) {
         
-        //Separo il comando da eventuali parametri
-        size_t splitPoint = input.find(' ');
-        if (splitPoint < input.size()) {
-            command = input;
-            parameter  = "";
-        } else {
-            command = input.substr(0, splitPoint);
-            parameter  = input.substr(splitPoint + 1);
-        }
-
+        splitInput(input, command, parameter);
 
         if (command == "set_time" && parameter != "") {
             try{
-                std::cout << set_time(decodeInput(parameter));
+                std::cout << dp->set_time(parameter);
             }
             catch(const std::runtime_error& e){
                 std::cerr << e.what() << std::endl;
@@ -53,11 +50,11 @@ int main() {
             std::cout << "Inserisci un comando>>";
         } 
         else if (command == "stats" && parameter == "") {
-            
+            std::cout << dp->stats();
             std::cout << "Inserisci un comando>>";
         } 
         else if (command == "reset" && parameter == "") {
-            std::cout << reset();
+            std::cout << dp->reset();
             std::cout << "Inserisci un comando>>";
         }
         else if (command == "Q" || command == "q"){
