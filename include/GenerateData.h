8138f32e2286eb125d33c95061be3abbaa1828de
rigonisimone05 <rigonisimone05@gmail.com>
2026-01-03 14:12:02 +0100

    Update GenerateData.h

--- include/GenerateData.h
+++ include/GenerateData.h
@@ -12,29 +12,31 @@
 #include "Vehicle.h"
 #include "Highway.h"
 
-class GenerateData : public Highway{
-public:
-    void startHighwaySimulation();
-    GenerateData(const std::string& highwayFile) : Highway(highwayFile);
-private:
-    std::unordered_map<std::string, bool> map;
-    const int HOURS_IN_SECOND = 3600 ;
-    const int NUM_VEHICLES = 10000;         
-    const double MIN_TIME_GAP = 0.5;        
-    const double MAX_TIME_GAP = 10.0;       
-    const int MIN_SPEED = 80;
-    const int MAX_SPEED = 190;
-    const int MIN_DURATION_MIN = 5;
-    const int MAX_DURATION_MIN = 15;
-
-    std::string generatePlate();
-    int randomInt(int min, int max);
-    double randomDouble(double min, double max);    
-    void generateRunsLine(std::ofstream& outFile, const Vehicle& vehicle);
-    void generatePassages(std::ofstream& outFile, const Vehicle& vehicle, 
-                         const std::vector<double>& gates, double kmEntry, double kmExit);
-    int findFirstGate(const std::vector<double>& gates, double kmEntry);
-    void generateProfile(Vehicle& v, double totalDistance);
+class GenerateData
+{
+    public:
+        void startHighwaySimulation();
+        GenerateData(const std::string& highwayFile) : Highway(highwayFile);
+    private:
+        Highway hw();
+        std::unordered_map<std::string, bool> map;
+        const int HOURS_IN_SECOND = 3600 ;
+        const int NUM_VEHICLES = 10000;         
+        const double MIN_TIME_GAP = 0.5;        
+        const double MAX_TIME_GAP = 10.0;       
+        const int MIN_SPEED = 80;
+        const int MAX_SPEED = 190;
+        const int MIN_DURATION_MIN = 5;
+        const int MAX_DURATION_MIN = 15;
+    
+        std::string generatePlate();
+        int randomInt(int min, int max);
+        double randomDouble(double min, double max);    
+        void generateRunsLine(std::ofstream& outFile, const Vehicle& vehicle);
+        void generatePassages(std::ofstream& outFile, const Vehicle& vehicle, 
+                             const std::vector<double>& gates, double kmEntry, double kmExit);
+        int findFirstGate(const std::vector<double>& gates, double kmEntry);
+        void generateProfile(Vehicle& v, double totalDistance);
 };
 
 #endif
