c0e3c36cb0a5fac82c7a5fb21b5036500c0f8785
rigonisimone05 <rigonisimone05@gmail.com>
2026-01-03 16:39:03 +0100

    Update Simulator.cpp

--- src/Simulator.cpp
+++ src/Simulator.cpp
@@ -17,19 +17,16 @@ int main()
 
     std::cout << "--- Starting Highway Simulator ---" << std::endl;
 
-    std::string highwayFile = "Data/Highway.txt";
+    std::string highwayFile = "Highway.txt";
     Highway hw(highwayFile);
     GenerateData simulator(highwayFile);
-    //creo i vettori
-
-    std::string runsFile = "Data/Runs.txt";
 
     double currentSimulationTime = 0.0;
 
     std::cout << "Generating 10000 vehicles..." << std::endl;
 
-    std::ofstream runsOut("Data/Runs.txt");
-    std::ofstream passOut("Data/Passages.txt");
+    std::ofstream runsOut("Runs.txt");
+    std::ofstream passOut("Passages.txt");
     simulator.startVehicleSimulation(runsOut, passOut);
 
     runsOut.close();
@@ -38,6 +35,7 @@ int main()
 
     return 0;
 }
+
 
 
 
