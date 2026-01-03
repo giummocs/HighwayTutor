d8c1cffd162e9a1eba4d4327b4d90fee05c0b24d
angeloyolo <angelo.you@studenti.unipd.it>
2026-01-03 15:31:12 +0100

    Update HighwayTester.cpp

--- src/HighwayTester.cpp
+++ src/HighwayTester.cpp
@@ -24,30 +24,43 @@ int main() {
 
     std::vector<double> svincoli = highway.getJunctions();
     std::vector<double> varchi = highway.getGates();
+
+    std::cout << "*****************************************************************\n\n";
     
     for (int i = 0; i < svincoli.size(); i++){
         double dist = svincoli[i];
-        std::cout << "*****************************************************************\n";
-        std::cout << "Svincolo " << i+1 << " distanza : " << dist << "km\n";
+        std::cout << "Svincolo " << i+1 << " distanza : " << dist << "km\n\n";
     }
+    
     std::cout << "\n##############################################################\n\n";
+    std::cout << "*****************************************************************\n\n";
+    
     for (int i = 0; i < varchi.size(); i++){
         double dist = varchi[i];
-        std::cout << "*****************************************************************\n";
-        std::cout << "Varco " << i+1 << " distanza : " << dist << "km\n";
+        std::cout << "Varco " << i+1 << " distanza : " << dist << "km\n\n";
     }
 
     std::cout << "//////////////////////////////////////////////////////////////\n\n";
-    std::cout << "=================== Trovare la distanza dei passaggi e i numeri suoi passaggi adiacenti ===================\n\n";
-
+    std::cout << "=================== Numero di svincoli e varchi ===================\n\n";
 
     char key;
+    
+    std::cout << "Inserisci la chiave (S per svincoli o V per varchi) di cui vuoi sapere il numero\n";
+
+    std::cin >> key;
+    int n = highway.getSize(key);
+
+    std::cout << "Numero di passaggi " << key << " : " << n << "\n\n";
+    
+    std::cout << "//////////////////////////////////////////////////////////////\n\n";
+    std::cout << "=================== Trovare la distanza dei passaggi ===================\n\n";
+
     int index;
     std::cout << "Inserisci la chiave (S per svincoli o V per varchi)" << "\n";
     std::cin >> key;
     std::cout << "Inserisci il numero del passaggio" << "\n";
     std::cin >> index;
-    std::cout << "Il passaggio " << key << " numero "<< index << " ha : \n";
+    std::cout << "\nIl passaggio " << key << " numero "<< index << " ha : \n";
     // secondo la convenzione del testo la numerazione dei passaggi cominciano da 1 e quindi l'indice del vettore sara' (numero - 1)
     index--;
 
@@ -67,9 +80,8 @@ int main() {
     std::cin >> index2;
     dist = highway.getDistanceBetween(key, index1-1, index2-1);
 
-    std::cout << "Distanza tra passaggi " << key << " numero " << index1 << " e numero " << index2 << " : " << dist << "km\n";
-
-    std::cout << "//////////////////////////////////////////////////////////////\n\n\n";
+    std::cout << "\nDistanza tra passaggi " << key << " numero " << index1 << " e numero " << index2 << " : " << dist << "km\n";
     
     return 0;
+
 }
