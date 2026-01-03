2c4f55f7a26ef340ea1c99cebab1f3610f532f5f
angeloyolo <angelo.you@studenti.unipd.it>
2026-01-03 15:06:19 +0100

    Add files via upload

--- include/Highway.h
+++ include/Highway.h
@@ -11,43 +11,34 @@
 #include <stdexcept>
 #include <unordered_map>
 
-struct HighwayNode {
-    double distance;
-    int prev;
-    int next;
-};
-
 class Highway {
 public:
-    
-
     Highway();
     Highway(const std::string& filename);
-    void loadFromFile(const std::string& filename);
-    const std::vector<HighwayNode>& getJunctions();
-    const std::vector<HighwayNode>& getGates();
+    
+    const std::vector<double>& getJunctions();
+    const std::vector<double>& getGates();
+
     double getDistance(char key, int index);
-    int getPrev(char key, int index);
-    int getNext(char key, int index);
     double getDistanceBetween(char key, int i, int j);
 
-
+    //Funzioni aggiuntivi non inerenti al nostro progetto ma potrebbero essere utili in un altro contesto
     void printJunctions();
     void printGates();
 
-protected:
+private:
     //Abbiamo scelto di implementare due vettori per differenziare al meglio varchi e svincoli, al posto di una struttura dati univoca per entrambi (struct).
     //Il che facilita molto la logica negli altri file: codice piu facile da leggere e alleggerito nella ricerca di varchi e svincoli.
-    std::unordered_map<char, std::vector<HighwayNode>> nodes;
+    std::unordered_map<char, std::vector<double>> nodes;
 
+    //Helper functions
+    void loadFromFile(const std::string& filename);
     bool isDouble(const std::string& s);
-    static bool compareDistance(const HighwayNode& a, const HighwayNode& b);
-    void setAdjacent(std::vector<HighwayNode>& a, std::vector<HighwayNode>& b);
-
 };
 
 
 #endif
+
 
 
 
