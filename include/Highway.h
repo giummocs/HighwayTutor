8549cfaaebad4a4975bbeb990114517f33c6804d
angeloyolo <angelo.you@studenti.unipd.it>
2026-01-04 16:11:51 +0100

    Update Highway.h

--- include/Highway.h
+++ include/Highway.h
@@ -21,31 +21,44 @@ public:
     void loadFromFile(const std::string& filename);
 
     //Funzioni getter 
-
-    //Restituisce il vettore con chiave 'S' (svincoli)
     const std::vector<double>& getJunctions();
-    //Restituisce il vettore con chiave 'V' (varchi)
+    //Restituisce il vettore con chiave 'S' (svincoli)
+    
     const std::vector<double>& getGates();
+    //Restituisce il vettore con chiave 'V' (varchi)
 
-    //Funzioni getter che accedono alla data chiave della mappa
-    //Lanciano eccezioni se key diverso da 'S' o 'V'
     int getSize(char key);
+    //Restituisce il numero di passaggi di un dato tipo (key)
+    //Lancia un eccezione se key diverso da 'S' o 'V'
+
     double getDistance(char key, int id);
+    //Restituisce la distanza del passaggio numero id del dato tipo (key)
+    //Lancia un eccezione se key diverso da 'S' o 'V'
+    //Lancia un eccezione se id non e' presente
+
     double getDistanceBetween(char key, int id1, int id2);
+    //Restituisce la distanza tra il passaggio numero id1 e numero id2 del dato tipo (key)
+    //Lancia un eccezione se key diverso da 'S' o 'V'
+    //Lancia un eccezione se id1 e id2 non sono presenti
 
     //Funzioni aggiuntive non inerenti al nostro progetto ma potrebbero essere utili in un altro contesto
     void printJunctions();
+    //Stampa a schermo la lista degli svincoli con la propria distanza
+
     void printGates();
+    //Stampa a schermo la lista dei varchi con la propria distanza
 
 private:
     //Variabile privato
     std::unordered_map<char, std::vector<double>> nodes;
 
-    //Helper function per il controllo se una stringa e' convertibile in double
+    //Helper functions
     bool isDouble(const std::string& s);
+    //Restituisce true se una stringa e' convertibile in double, false altrimenti
 };
 
 #endif
+
 
 
 
