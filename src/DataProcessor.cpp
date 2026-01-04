4154b8e560468ecec4b85589ca4162ba41a111c4
giummocs <gianmaria.stefani@studenti.unipd.it>
2026-01-05 00:15:21 +0100

    Update DataProcessor.cpp

--- src/DataProcessor.cpp
+++ src/DataProcessor.cpp
@@ -18,11 +18,11 @@ void DataProcessor::processData(){
     double totalTime = 0.0;
 
     //Complessita' totale O(n), dove n e' il numero delle righe del file passages.txt
-    //Scorro la mappa con chiave plate, cosi da scorrere un veicolo per volta
+    //Scorre la mappa con chiave plate, cosi da scorrere un veicolo per volta
     //Ogni elemento della mappa, cioè ogni veicolo, corrisponde a un vettore che contiene tutti i varchi che il veicolo ha attraversato
     for (const auto& mapElement : passages) {
         
-        //Ordino i passaggi per id, cosi da riempire le mappe con gli id gia ordinati
+        //Ordina i passaggi per id, cosi da riempire le mappe con gli id gia ordinati
         std::sort(mapElement.second.begin(), mapElement.second.end(), compareId);
 
         //Numero di varchi che il veicolo ha attraversato
@@ -31,7 +31,7 @@ void DataProcessor::processData(){
         //Salvo gia il primo varco
         updateStat(mapElement.second[0].id, mapElement.second[0].time);
 
-        //Scorro tutti i varchi attraversati da una macchina precisa, analizzandone due consecutivi alla volta, tramite i e i+1
+        //Scorre tutti i varchi attraversati da un veicolo preciso, analizzandone due consecutivi alla volta, tramite i e i+1
         for(std::size_t i=0; i+1 < gatesNumber; i++){
             int idGate1 = mapElement.second[i].id;
             int idGate2 = mapElement.second[i+1].id;
@@ -48,7 +48,7 @@ void DataProcessor::processData(){
                 throw std::runtime_error("Errore! File non valido: errato tempo di passaggio di due varchi!");
             }
 
-            //Salvo i dati in violations, utile a set_time
+            //Salva i dati in violations, utile a set_time
             double distanceDifference = hw.getDistanceBetween('V', idGate1, idGate2); //Distanza tra due varchi
             double timeDifference = timeGate1 - timeGate2; //Differenza di tempo tra i due varchi
             double averageVelocity = distanceDifference / (timeDifference/SECONDS_IN_HOURS);
@@ -58,7 +58,7 @@ void DataProcessor::processData(){
                 violations[mapElement.first].push_back({idGate1, idGate2, averageVelocity, timeGate1, timeGate2});
             }
 
-            //Salvo i dati in statistics, utile a stats
+            //Salva i dati in statistics, utile a stats
             updateStat(idGate2, timeGate2);
 
             //Aggiungo la distanza e il tempo percorso al totale (utile al comando stats)
@@ -71,7 +71,7 @@ void DataProcessor::processData(){
 
 
 std::string DataProcessor::set_time(const std::string& input){
-    //Chiamo decodeInput per decodificare l'input
+    //Chiama decodeInput per decodificare l'input
     int addTime = decodeInput(input);
 
     if(addTime <= 0){
@@ -81,12 +81,10 @@ std::string DataProcessor::set_time(const std::string& input){
     double newTime = currentTime + addTime;
     std::string output = "\nInfrazioni commesse tra gli istanti "+std::to_string(currentTime)+" e "+std::to_string(newTime)+":";
 
-    
+    //Scorre tutte le violazioni
     for (const auto& mapElement : violations) {
-
         std::size_t gatesNumber = mapElement.second.size();
 
-        //Scorro tutti i gate attraversati da una macchina precisa, analizzandone due consecutivi alla volta tramite i e i+1
         for(std::size_t i=0; i+1 < gatesNumber; i++){
 
             int gateStartId = mapElement.second[i].gateStartId;
@@ -95,7 +93,7 @@ std::string DataProcessor::set_time(const std::string& input){
             double gateEndTime = mapElement.second[i].gateEndTime;
             double averageVelocity = mapElement.second[i].averageVelocity;
 
-            //Controllo che tutti i due gate di riferimento siano dentro il range di tempo richiesto dal parametro addTime
+            //Considera solo le violazioni che rientrano nel range di tempo richiesto da input
             if(gateStartTime > currentTime && gateEndTime < newTime){
                 output += "\nInfrazione";
                 output += "\nTarga: "+mapElement.first+"\nTratta: varco "+std::to_string(gateStartId)+" - varco "+std::to_string(gateEndId)+"\nVelocità media: "+std::to_string(averageVelocity)+"\nIstante di passaggio varco "+std::to_string(gateStartId)+": "+std::to_string(gateStartTime)+"\nIstante di passaggio varco "+std::to_string(gateEndId)+": "+std::to_string(gateEndTime)+"\n";
@@ -105,6 +103,7 @@ std::string DataProcessor::set_time(const std::string& input){
 
     if(violations.size() == 0) output += "\nNessuna infrazione.";
 
+    //Imposta il nuovo tempo
     currentTime = newTime;
 
     return output;
@@ -114,13 +113,14 @@ std::string DataProcessor::stats(){
     std::string output = "\nStatistiche autostrada:";
     int totalGates = hw.getSize('V');
 
+    //Scorre tutti i varchi dell'autostrada
     for (std::size_t i=0; i < totalGates; i++) {
         int vehiclesNumber = statistics[i].vehiclesNumber;
         double minTime = statistics[i].minTime;
         double maxTime = statistics[i].maxTime;
         double vehiclePerMinute = 0.0;
 
-        //Se sono transitati piu di un veicolo calcolo vehiclePerMinute, altrimenti non e' possibile calcolare un valore valido e lascio 0
+        //Se sono transitati piu di un veicolo calcola vehiclePerMinute, altrimenti non e' possibile calcolare un valore valido e lascia 0
         if(vehiclesNumber > 1){
             vehiclePerMinute = vehiclesNumber / ((maxTime-minTime)/SECONDS_IN_MINUTES);
         }
@@ -139,13 +139,12 @@ std::string DataProcessor::reset(){
     return "\nSistema azzerrato con successo!";
 }
 
-//Traduce il tempo ricevuto come parametro da stringa a intero
 int DataProcessor::decodeInput(const std::string& input){
     bool hasM = false;
     std::string numberPart;
     int result;
 
-    //Controllo ultimo carattere e tolgo m
+    //Controlla l'ultimo carattere per verificare la presenza di 'm'
     if (input.back() == 'm') {
         hasM = true;
         numberPart = input.substr(0, input.size() - 1);
@@ -156,17 +155,16 @@ int DataProcessor::decodeInput(const std::string& input){
     //La parte numerica deve esistere
     if (numberPart.empty()) throw std::runtime_error("Errore! File non valido: ");
 
-    //Conversione
+    //Conversione, se la parte numerica non è valida lancia l'eccezione
     try {
         result = std::stoi(numberPart);
     } catch (...) {
         throw std::runtime_error("Errore! File non valido: ");
     }
-    
 
-    //Se aveva m, moltiplico per 60 per convertire in secondi
+    //Se è presente m, moltiplico per 60 per convertire in secondi
     if (hasM) {
-        result *= 60;
+        result *= SECONDS_IN_MINUTES;
     }
 
     return result;
@@ -179,10 +177,15 @@ static bool compareId(const PassageByPlate& p1, const PassageByPlate& p2) {
 
 
 void DataProcessor::updateStat(int id, double time){
+    //Se il varco con questo id non era già presente nella mappa lo aggiunge
     Statistic& stat = statistics[id];
+
+    //Incrementa il numero di veicoli che hanno attraversato questo varco
     stat.vehiclesNumber++;
-    stat.minTime = std::min(stat.minTime, time); //Salvo istante minimo
-    stat.maxTime = std::max(stat.maxTime, time); //Salvo istante massimo
+    
+    //Salva l'istante minimo e massimo in cui una macchina ha attraversato questo varco
+    stat.minTime = std::min(stat.minTime, time); 
+    stat.maxTime = std::max(stat.maxTime, time); 
 }
 
 
@@ -219,6 +222,7 @@ void DataProcessor::loadFromFile(const std::string& filename){
     file.close();
 
 }
+
 
 
 
